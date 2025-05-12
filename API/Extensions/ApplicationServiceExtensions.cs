95d8e4a03c2334602d8d14b2f4241c1c83e6f57b
benjamin-achouche <benjaminachouche@gmail.com>
2025-05-12 23:55:42 +0200

    Added API messaging feature

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -17,6 +17,7 @@ public static class ApplicationServiceExtensions {
     services.AddScoped<ITokenService, TokenService>();
     services.AddScoped<IUserRepository, UserRepository>();
     services.AddScoped<ILikeRepository, LikeRepository>();
+    services.AddScoped<IMessageRepository, MessageRepository>();
     services.AddScoped<IPhotoService, PhotoService>();
     services.AddScoped<LogUserActivity>();
     services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
